9aa37a87c4555c2946e942b571c8d202c9c553d7
mohd akram <akrammohd421@gmail.com>
2025-12-03 13:13:00 +0530

    feat - assignment operator

--- main.go
+++ main.go
@@ -1,7 +1,100 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main(){
-	fmt.Printf("hello, Go");
-}
+// func main(){
+// 	fmt.Printf("hello, Go");
+// }
+
+// add 
+// package main 
+// import "fmt"
+// func main(){
+// 	var x = 5
+
+// 	x +=3
+// 	fmt.Println(x);
+// }
+
+// subtract 
+
+// package main 
+// import "fmt"
+// func main(){
+// 	var x = 5
+
+// 	x -=3
+// 	fmt.Println(x);
+// }
+
+// multiply
+
+// package main 
+// import "fmt"
+// func main(){
+// 	var x = 5
+
+// 	x *=3
+// 	fmt.Println(x);
+// }
+
+// divide
+// package main 
+// import "fmt"
+// func main(){
+// 	var x = 5
+
+// 	x /=3
+// 	fmt.Println(x);
+// }
+
+// modulus
+// package main 
+// import "fmt"
+// func main(){
+// 	var x = 5
+
+// 	x %=3
+// 	fmt.Println(x);
+// }
+
+// x is a value: 10
+
+// p is a pointer to x (it stores the memory address of x)
+
+// & &&&&&&&&&&&&&&&
+
+// and operator
+
+// package main
+
+// import "fmt"
+
+// func main(){
+// 	a:= 5
+// 	b:= &a 
+// 	fmt.Println("a=", a)
+// 	fmt.Println("b address=", b)
+// 	fmt.Println("b= actual value using pointer", *b)
+// }
+
+
+// output:-
+
+// a = 5
+// b = 0xc000014090   <-- memory address
+// *b = 5
+
+// x := 10
+// p := &x   // p points to x
+
+// *p = 20   // change value using pointer
+
+// fmt.Println(x)  // 20
+
+// -------------------------------------
+
+
+
+
+
