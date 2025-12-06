21ebd5c622b35508ffdba6ce153304aa31b9d35f
mohd akram <akrammohd421@gmail.com>
2025-12-06 23:55:28 +0530

    feat - Switch { Single Case }

--- main.go
+++ main.go
@@ -445,3 +445,48 @@
 //     fmt.Println("Num is less than 10.")
 //   }
 // }
+
+
+
+// The switch Statement--------------------
+
+
+
+// Single-Case switch Syntax
+// Syntax
+// switch expression {
+// case x:
+//    // code block
+// case y:
+//    // code block
+// case z:
+// ...
+// default:
+//    // code block
+// }
+
+
+
+// package main
+// import ("fmt")
+
+// func main() {
+//   day := 4
+
+//   switch day {
+//   case 1:
+//     fmt.Println("Monday")
+//   case 2:
+//     fmt.Println("Tuesday")
+//   case 3:
+//     fmt.Println("Wednesday")
+//   case 4:
+//     fmt.Println("Thursday")
+//   case 5:
+//     fmt.Println("Friday")
+//   case 6:
+//     fmt.Println("Saturday")
+//   case 7:
+//     fmt.Println("Sunday")
+//   }
+// }
