71e4a39a11e886a450e3d569dd1ee6df50119644
mohd akram <akrammohd421@gmail.com>
2025-12-09 23:49:58 +0530

    feat - function returns / return values , function return example

--- main.go
+++ main.go
@@ -774,4 +774,30 @@
 //   familyName("Anja", 30)
 // }
 
+// function returns
 
+// package main
+// import ("fmt")
+
+// func myFunction(x int, y int) int {
+//   return x + y
+// }
+
+// func main() {
+//   fmt.Println(myFunction(1, 2))
+// }
+
+
+// Named Return Values
+
+// package main
+// import ("fmt")
+
+// func myFunction(x int, y int) (result int) {
+//   result = x + y
+//   return
+// }
+
+// func main() {
+//   fmt.Println(myFunction(1, 2))
+// }
