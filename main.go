ff7f3ab2d76bc4e38a8006f3c4b11523469a9185
mohd akram <akrammohd421@gmail.com>
2025-12-15 06:10:56 +0530

    feat - struct

--- main.go
+++ main.go
@@ -971,3 +971,60 @@
 // 	fmt.Println(s)
 // }
 
+// package main
+// import ("fmt")
+
+// func myMessage(ext int) int {
+
+// if ext == 11{
+// 	return 0
+// }
+// fmt.Println(ext)
+// return myMessage(ext + 1)
+
+// }
+
+// func main(){
+// 	myMessage(1)
+// }
+
+// struct in go----------------------------
+
+// package main
+// import ("fmt")
+
+// type Person struct{
+// 	name string
+// 	age int
+// 	job string
+// 	salary int
+// }
+
+// func main(){
+// 	var pers1 Person
+// 	var pers2 Person
+
+// pers1.name = "hege"
+// pers1.age = 18
+// pers1.job = "teacher"
+// pers1.salary = 28000
+
+// pers2.name = "salman"
+// pers2.age = 24
+// pers2.job = "film maker"
+// pers2.salary = 39000
+
+// fmt.Println("name",pers1.name)
+// fmt.Println("AGE", pers1.age)
+// fmt.Println("job", pers1.job)
+// fmt.Println("salary", pers1.salary)
+
+// fmt.Println("name", pers2.name)
+// fmt.Println("age", pers2.age)
+// fmt.Println("job", pers2.job)
+// fmt.Println("salary", pers2.salary)
+
+// fmt.Println("salary", pers2)
+
+
+// }
